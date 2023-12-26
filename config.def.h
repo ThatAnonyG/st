a1520f293b1d651a9e237f45fdebdf5ef8a879e5
Ratul Saha <contact@ratulsaha.me>
2023-12-26 11:40:49 +0530

    fix: fixed font issue with spacing and size

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "JetBrainsMono Nerd Font Mono:medium:size=10:antialias=true:autohint=true:style=Regular";
+    "JetBrainsMono Nerd Font Mono:size=10:antialias=true:autohint=true:style=Regular";
 static int borderpx = 8;
 
 /*
@@ -27,8 +27,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 0.8;
-static float chscale = 1.0;
+static float cwscale = 0.85;
+static float chscale = 0.95;
 
 /*
  * word delimiter string
